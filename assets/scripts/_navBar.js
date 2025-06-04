cb72e3f3e5dcebfa70addb38e8300212261e62e0
L-d3v <lmjeronimo11@gmail.com>
2025-06-04 20:55:05 -0300

    Melhorias de desempeno, SEO

--- assets/scripts/_navBar.js
+++ assets/scripts/_navBar.js
@@ -1,9 +1,12 @@
 function openMenu() {
-  const navMenu = document.querySelector('.nav__mobile');
-  if (navMenu.classList.contains('active')) {
-      navMenu.classList.remove('active');
-
+  const navMenu = document.querySelector(".nav__mobile");
+  if (navMenu.classList.contains("active")) {
+    navMenu.classList.remove("active");
   } else {
-      navMenu.classList.add('active');
+    navMenu.classList.add("active");
   }
-};
+}
+
+document.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => toggleMenu());
+});
